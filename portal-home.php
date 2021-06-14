3a8e026ac936bc28fbaad6e805c2156b8e36eb07
Neil Master <yeehawlerz101@gmail.com>
2021-06-14 12:06:31 -0500

    Update portal-home.php

--- portal-home.php
+++ portal-home.php
@@ -1,30 +1,25 @@
-<?php
-/*
-Template Name: Portal Home 
-<!-- todo: Get IBO Contract Signed -->
-<!-- todo: Get Referral Pannels Done -->
+<?php 
+/* Template Name: Portal Home  */ 
+?>
 
-<?php update_field('first_name', $value, 'user_'.$uuid);?> */ 
- ?>
-
-
+<?php get_header(); ?>
+<div id="primary" class="content-area">
 	<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-	<style type="text/css">
-	</style>
-
-	<div id="primary" class="bg-gray-50">
-		<div id="content" role="main">
-			
-			test template
-		</div>
-		</div>
-
-	<p>
-		<?php the_content(); ?>
-	</p>
-	<?php endwhile; // end of the loop. ?>
-		</div>
-		<!-- #content -->
-		</div> 
-		<!-- #primary  -->
-<?php get_footer();    ?>
+                <main id="main" class="site-main" role="main">
+                                <?php
+                                // Start the loop.
+                                while ( have_posts() ) : the_post();
+                                                // Include the page content template.
+                                                get_template_part( 'template-parts/content', 'page' );
+                                                // If comments are open or we have at least one comment, load up the comment template.
+                                                if ( comments_open() || get_comments_number() ) {
+                                                                comments_template();
+                                                }
+                                                // End of the loop.
+                                endwhile;
+                                ?>
+                </main><!-- .site-main -->
+                <?php get_sidebar( 'content-bottom' ); ?>
+</div><!-- .content-area -->
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
