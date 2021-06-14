be8534bda4818ffe0dc9e875a4f21d0676289d90
Neil Master <yeehawlerz101@gmail.com>
2021-06-14 10:50:20 -0500

    Create portal-home.php

---
+++ portal-home.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Template Name: Portal Home 
+<!-- todo: Get IBO Contract Signed -->
+<!-- todo: Get Referral Pannels Done -->
+
+<?php update_field('first_name', $value, 'user_'.$uuid);?> */ 
+ ?>
+
+
+	<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+	<style type="text/css">
+	</style>
+
+	<div id="primary" class="bg-gray-50">
+		<div id="content" role="main">
+			
+			test template
+		</div>
+		</div>
+
+	<p>
+		<?php the_content(); ?>
+	</p>
+	<?php endwhile; // end of the loop. ?>
+		</div>
+		<!-- #content -->
+		</div> 
+		<!-- #primary  -->
+<?php get_footer();    ?>
